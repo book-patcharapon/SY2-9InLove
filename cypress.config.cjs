8f6d6ba664408b421e80615a49e5d5117dc74593
book-patcharapon <book13135555@gmail.com>
2023-08-28 01:21:18 +0700

    sp1 reall

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl:"http://localhost:5173/",
+    // baseUrl:"http://localhost:5173/",
     // baseUrl:"http://ip22sy2.sit.kmutt.ac.th:3000/",
-    // baseUrl: "http://intproj22.sit.kmutt.ac.th:80/sy2/",
+    baseUrl: "http://intproj22.sit.kmutt.ac.th:80/sy2/",
     experimentalSessionAndOrigin: true
   },
 });
